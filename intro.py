4781234d0719f1e7829264378997a24cdabeac1e
Teddili <zawyehtet7777@gmail.com>
2025-10-28 21:10:21 +0900

    used conditions and arrays

--- intro.py
+++ intro.py
@@ -1,19 +1,14 @@
-name = input("What is your name? ")
-age = input("How old are you? ")
-age = int(age)
-weight = input("What is your weight in kg? ")
-weight = float(weight)
-height = input("What is your height in cm? ")
-height = float(height) / 100  
-mbi = (weight / (height ** 2)) 
-def mbi_category(mbi):
-    if mbi < 18.5:
-        return "underweight"
-    elif 18.5 <= mbi < 24.9:
-        return "normal weight"
-    elif 25 <= mbi < 29.9:
-        return "overweight"
-    else:
-        return "dangerously overweight"
-print("Hello,", name + "! You are", age, "years old.")
-print(f"Your  MBI is {mbi:.1f} you are {mbi_category(mbi)}")
+month = int(input("What is your birth month? "))
+day = int(input("What is your birth day? "))
+year = int(input("What is your birth year? "))
+print("you were born on "+str(month)+"/"+str(day)+"/"+str(year))
+if month>12 or day>31 or year>2024:
+    print("Error: Invalid date")
+elif month==2 and day>29:
+    print("Error: Invalid date")
+elif month in [4,6,9,11] and day>30:
+    print("Error: Invalid date")
+else:
+    print("Thank you for providing your birth date!")
+    age = 2024 - year
+    print("You are approximately "+str(age)+" years old.")
