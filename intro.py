3e28830f60a1bfa082b81f70c8c1a9d46dcd284c
Teddili <zawyehtet7777@gmail.com>
2025-10-29 10:22:30 +0900

    used array used enumerate instesd of range and add some index

--- intro.py
+++ intro.py
@@ -1,14 +1,3 @@
-month = int(input("What is your birth month? "))
-day = int(input("What is your birth day? "))
-year = int(input("What is your birth year? "))
-print("you were born on "+str(month)+"/"+str(day)+"/"+str(year))
-if month>12 or day>31 or year>2024:
-    print("Error: Invalid date")
-elif month==2 and day>29:
-    print("Error: Invalid date")
-elif month in [4,6,9,11] and day>30:
-    print("Error: Invalid date")
-else:
-    print("Thank you for providing your birth date!")
-    age = 2024 - year
-    print("You are approximately "+str(age)+" years old.")
+students=["Alice", "Bob", "Charlie", "David"]
+for i,student in enumerate(students):
+    print(f"This is student no {i+1}: {student}")
