c183b58f85d7ff7f16561fd5d8595ce706a6d159
Teddili <zawyehtet7777@gmail.com>
2025-11-04 22:07:52 +0900

    created class and one inherited and override parient

--- intro.py
+++ intro.py
@@ -1,15 +1,12 @@
-class student:
-    def __init__(self,name,age,grade):
-        self.name =name
-        self.age=age
-        self.grade=grade
-    def output(self):
-        print(f"the student name {self.name} { self.age} has {self.grade} grade! ")
-        
-s1=student("mgmg",23,"A")
-s2=student("hlahla",22,"E")
-s3=student("myamya",23,"C")
+class manager:
+    def salary(self):
+        print("this is your slaray for the month and keep trying!!!")
+class employee(manager):
+    def salary(self):
+        print("is this all??")
 
-s1.output()
-s2.output()
-s3.output()
+a=manager()
+b=employee()
+ 
+a.salary()
+b.salary()
