f7c98603c5d839b7794ad6e2d099b10c58129b20
Teddili <zawyehtet7777@gmail.com>
2025-11-08 14:35:17 +0900

    used pandas nad csv file to shown summary table

--- intro.py
+++ intro.py
@@ -1,15 +1,7 @@
-# console_notepad.py
-from pathlib import Path
+import pandas as pd
 
-fname = input("File name (Enter=notes.txt): ").strip() 
-print("Type your notes. Type ::end on its own line to finish.\n")
-
-lines = []
-while True:
-    line = input()
-    if line.strip() == "::":
-        break
-    lines.append(line + "\n")
-
-Path(fname).write_text("".join(lines), encoding="utf-8")
-print(f"Saved to {Path(fname).resolve()}")
+df = pd.read_csv('sales.csv')
+print(df.head())
+print(df['price'].mean())
+grouped = df.groupby('category')['sales'].sum()
+print(grouped)
